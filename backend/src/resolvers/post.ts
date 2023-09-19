36c9ac4727b26ef13b33435039ba14a84ad231a1
Organique0 <grabnar.luka@gmail.com>
2023-09-19 08:09:00 +0200

    posts api endpoint

--- backend/src/resolvers/post.ts
+++ backend/src/resolvers/post.ts
@@ -1,4 +1,4 @@
-import { Resolver, Query } from "type-graphql";
+import { Resolver, Query, Arg, Int, Args, Mutation, Ctx } from "type-graphql";
 import { Post } from "@generated/type-graphql"
 import { PrismaClient, Prisma } from "@prisma/client";
 import { DefaultArgs } from "@prisma/client/runtime/library";
@@ -10,8 +10,62 @@ type MyContext = {
 @Resolver()
 export class PostResolver {
     @Query(() => [Post])
-    async posts({ prisma }: MyContext) {
+    async posts({ prisma }: MyContext): Promise<Post[]> {
         const posts = await prisma.post.findMany();
         return posts;
+    }
+
+    @Query(() => Post, { nullable: true })
+    async post(
+        @Arg("id", () => Int) id: number,
+        { prisma }: MyContext
+    ): Promise<Post | null> {
+        return await prisma.post.findFirst({
+            where: {
+                id
+            }
+        });
+    }
+
+    @Mutation(() => Post)
+    async createPost(
+        @Arg("title") title: string,
+        @Arg("text") text: string,
+        @Ctx() { prisma }: MyContext
+    ): Promise<Post> {
+        return await prisma.post.create({
+            data: {
+                title,
+                text
+            }
+        });
+    }
+
+    @Mutation(() => Post)
+    async updatePost(
+        @Arg("title") title: string,
+        @Arg("id") id: number,
+        @Ctx() { prisma }: MyContext
+    ): Promise<Post> {
+        return await prisma.post.update({
+            where: {
+                id
+            },
+            data: {
+                title
+            }
+        });
+    }
+    @Mutation(() => Post)
+    async deletePost(
+        @Arg("id") id: number,
+        @Ctx() { prisma }: MyContext
+    ): Promise<Boolean> {
+        await prisma.post.delete({
+            where: {
+                id
+            },
+        });
+        return true;
     }
 }
