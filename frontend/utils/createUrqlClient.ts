f6118ce34e3bd6f26691fdfe1ca46a847626677c
Organique0 <grabnar.luka@gmail.com>
2023-09-23 03:52:28 +0200

    urql error handling

--- frontend/utils/createUrqlClient.ts
+++ frontend/utils/createUrqlClient.ts
@@ -2,11 +2,42 @@
 //The main difference between client and server URQL code are the IMPORTS.
 //I don not know exactly why but otherwise it does not work properly
 //This is how it is done in the official documentation
-import { fetchExchange, createClient, SSRExchange } from '@urql/next';
+import { fetchExchange, createClient, SSRExchange, Exchange } from '@urql/next';
 import { cacheExchange } from '@urql/exchange-graphcache';
+import { } from "@urql/core";
 import { betterUpdateQuery } from './betterUpdateQuery';
 import { LoginMutation, UserQuery, UserDocument, RegisterMutation, LogoutMutation } from '@/graphql/operations';
+import { pipe, tap } from "wonka";
+import { useRouter } from 'next/navigation';
+import { useUserQuery } from '@/graphql/queries/user.hooks';
+import { useEffect } from 'react';
+import useIsAuth from './useIsAuth';
 
+
+export const errorExchange: Exchange = ({ forward }) => (ops$) => {
+    //useRouter now woks, but only if you remove useMemo when calling getUrqlClient in providers
+    const router = useRouter();
+
+    return pipe(
+        forward(ops$),
+        tap(value => {
+            const { error } = value
+            if (error?.message.includes("not logged in")) {
+                /* 
+                this is the only way I was able to make this work without useRouter hook
+                but, this way is worse since Next router is faster
+                this should only be used when you want to refresh the page
+                
+                window.location.pathname = "/login";
+                */
+
+                //use next.js router that does not refresh the page
+                router.push("/login")
+
+            }
+        }),
+    )
+};
 
 export const getUrqlClient = (ssr: SSRExchange) => {
     return createClient({
@@ -63,7 +94,7 @@ export const getUrqlClient = (ssr: SSRExchange) => {
                     }
                 }
             }
-        }), ssr, fetchExchange],
+        }), ssr, errorExchange, fetchExchange],
     });
 }
 //export const { getClient } = registerUrql(getUrqlClient);
